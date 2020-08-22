aed81d9a1656f06757aa742609972326da2a9098
Milton Bedah <miltonbedah@gmail.com>
2020-08-22 15:59:35 +0300

    Safely integrated both nodemailer and mongoDB with the app

--- app.js
+++ app.js
@@ -1,59 +1,85 @@
-const express = require('express');
-const path = require('path');
-const exphbs = require('express-handlebars');
-const nodemailer = require('nodemailer');
-const mongoose = require('mongoose');
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const nodemailer = require("nodemailer");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const Contact = require('./contacts/contactModel');
+const Contact = require("./contacts/contactModel");
 //using local database
-const db = mongoose.connect('mongodb://localhost/contact',
-    {
-        useUnifiedTopology: true,
-        useNewUrlParser: true
-    }
-);
+const db = mongoose.connect("mongodb://localhost/contact", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
 
 //set the template engine
-app.engine('.hbs', exphbs({defaultLayout: 'main',extname: '.hbs' }));
-app.set('view engine', '.hbs');
+app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
 
 //parser middlware
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 // setting a static folder
-app.use('/public',express.static('public'));
+app.use("/public", express.static("public"));
 
-app.get('/', (req, res) => {
-    res.render('contact');
+app.get("/", (req, res) => {
+  res.render("contact");
 });
 
-app.post('/send', (req, res) =>{
-    const details = `
+app.post("/send", (req, res) => {
+  const customerDetails = `
         <h1>Customer Enquiry</h1>
         <ul>
             <li>Name: ${req.body.username}</li>
             <li>Telephone: ${req.body.telephone}</li>
             <li>Customer's email: ${req.body.email}</li>
-            <h3> Message is: </h3> <li> ${req.body.message}</li>
+            <h3> Message is: </h3> <p> ${req.body.message}</p>
             
         </ul>    
     `;
-    const contact = new Contact(req.body)
 
-    contact.save((err, success) => {
-        if (err){
-            return console.log(err);
-        }
-        if(success){
-            return res.status(201);
-            console.log('Data Succesfully added');
-        }
+  // mongoDB intergration
+  const contact = new Contact(req.body);
+  contact.save((err, success) => {
+    if (err) {
+      return console.log(err);
+    }
+    return res.status(201);
+    console.log("Data Succesfully added");
+  });
+
+  async function main() {
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        type: "OAuth2",
+        user: "miltonbedah@gmail.com",
+        accessToken:
+          "ya29.a0AfH6SMDXDC0nnCOnzZe-rcM0eyouScRz-gBSXqu0XwTbmt4cUNtxDArRD2U9HguYDpjzrQL6U8YUEot0gHQR67CgEkbAQOs8MGlYRG5oR9vfUzV0k5YaG0s2-q6jDlX5lnEGk5joIdFwgcsHTxSVwSLltjW_8C3EuT8",
+      },
     });
-    console.log(details);
 
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"Mars Rocks🥨" miltonbedah1485@gmail.com', // sender address
+      to: "miltonbedah@gmail.com",
+      subject: "Contact Details", // Subject line
+      html: customerDetails, // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+    res.render("contact", { msg: "Email sent" });
+  }
+
+  main().catch(console.error);
 });
 
-app.listen(PORT, ()=> `App running on port ${PORT}`);
+app.listen(PORT, () => `App running on port ${PORT}`);
